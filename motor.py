2c35c3a95a077d015f1444d767c432c59b6fe1c7
Lars Fendell <larsf@axelbrott.se>
2018-06-23 23:12:49 +0200

    Working positioning for altitude motor

--- motor.py
+++ motor.py
@@ -1,3 +1,4 @@
+import logging
 import Adafruit_ADS1x15
 import pysolar
 import datetime
@@ -16,9 +17,11 @@ class Motor(object):
         self.posAdress = PosAdress
         self.pos = 0
         self.SPR = spr
+        self.pulse_delay = 0.0005
 
 
     def get_pos(self):
+        """Returns pos of motor read from pot adc"""
         self.pos = self.adc.read_adc(self.posAdress, self.GAIN)
         self.pos = int(self.map(self.pos, 0, 26140, 0, 360))
         return self.pos
@@ -26,14 +29,23 @@ class Motor(object):
     def map(self, x, in_min, in_max, out_min, out_max):
         return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
-    def move(self, dir, sunPos):
+    def move(self, set_pos, CCW=False):
         """Run motor, takes in direction and suns position"""
-        GPIO.output(self.enablePin, GPIO.HIGH)
-        print(self.enablePin)
-        delay = 0.02
-        while(sunPos > self.get_pos()):
-                GPIO.output(self.stepPin, GPIO.HIGH)
-                time.sleep(delay)
-                GPIO.output(self.stepPin, GPIO.LOW)
-                time.sleep(delay)
         GPIO.output(self.enablePin, GPIO.LOW)
+        if not CCW:
+            GPIO.output(self.dirPin, GPIO.HIGH)
+            while(set_pos > self.get_pos()):
+                self.pulse_motor()
+        else:
+            GPIO.output(self.dirPin, GPIO.LOW)
+            while(set_pos < self.get_pos()):
+                self.pulse_motor()
+
+        GPIO.output(self.enablePin, GPIO.HIGH)
+
+    def pulse_motor(self):
+        """pulse for stepper"""
+        GPIO.output(self.stepPin, GPIO.HIGH)
+        time.sleep(self.pulse_delay)
+        GPIO.output(self.stepPin, GPIO.LOW)
+        time.sleep(self.pulse_delay)
--- sun_seeker.py
+++ sun_seeker.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 #Sunseeker follows the sun and using pysolar and rpi2
 #Author: Lars Fendell
+import logging
 import time
 from motor import Motor
 import configparser
@@ -11,8 +12,6 @@ import RPi.GPIO as GPIO
 class App(object):
     date = datetime.datetime.now()
     config = configparser.ConfigParser()
-    CW = 0
-    CCW = 1
     def __init__(self):
         self.config.read('config.ini')
         self.longitude = float(self.config['SETTINGS']['longitude'])
@@ -37,12 +36,22 @@ class App(object):
     def update(self):
         """Update App ,general things to do every cycle"""
         self.get_sun_pos()
-        if(self.sunAltitude > self.altitudeMotor.get_pos()):
-            self.altitudeMotor.move(self.CW, self.sunAltitude)
-        if(self.sunAzimuth > self.azimuthMotor.get_pos()):
-            self.azimuthMotor.move(self.CW, self.sunAzimuth)
-        print('Sun altitude: {0:1f}\n Sun azimuth: {1:1f}\n'.format(self.sunAltitude, self.sunAzimuth))
+        logging.info('Sun altitude: {0:1f}\n Sun azimuth: {1:1f}\n'.format(self.sunAltitude, self.sunAzimuth))
+        self.check_altitude()
+        self.check_azimuth()
+        self.altitudeMotor.move(10, True)
 
+
+    def check_altitude(self):
+        print("Altitude motor pos:{} Sun altitude:{}".format(self.altitudeMotor.get_pos(), self.sunAltitude))
+        if(self.sunAltitude > self.altitudeMotor.get_pos()):
+            self.altitudeMotor.move(self.sunAltitude, False)
+
+    def check_azimuth(self):
+        print("Azimuth motor pos:{} Sun azimuth:{}".format(self.azimuthMotor.get_pos(), self.sunAzimuth))
+        if(self.sunAzimuth > self.azimuthMotor.get_pos()):
+            self.azimuthMotor.move(self.sunAzimuth, False)
+ 
     def setup_pins(self):
         #Ställ in GPIO utgångar
         GPIO.setmode(GPIO.BCM)
